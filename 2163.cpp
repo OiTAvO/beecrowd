9b4d8df5653bb989cdfde17f8df04ca09ae91da1
Otávio Augusto <otavio.porto@fatec.sp.gov.br>
2019-03-10 17:31:43 -0300

    inserido comentários ao arquivo

--- 2163.cpp
+++ 2163.cpp
@@ -1,65 +1,55 @@
-// Copyright OAMP
-#include <iostream>
-#include <cstdio>
-#include <string>
-#include <cstring>
-#include <cstdlib>
-#include <stack>
+// Copyright OAMP - tempo 0.052s 
+#include <cstdio>  // Entrada e saída padrão
+#include <cstring>  // Utilizado p/ strtok
+#include <cstdlib>  // Utilizado p/ atoi
+#include <stack>  // Container
 
-using std::getline;
+struct coord {int y = -1, x = -1;};  // Estrutura para coordenada
 
 int main() {
-  unsigned int n, m, count, posY;
-  typedef std::pair<int, int> coord;
-  coord xy;
-  std::stack<coord> pilha;
-  char* cPtr;
-  char str[7000];
+  unsigned int n, m, count, posY;  // linha, coluna, contador e Pos. Y
+  std::stack<coord> pilha ;  // Container de coordenada
+  coord xy, stdXY;  // estrutura p/ resultado final e estrutura padrão
+  char* cPtr;  // ponteiro p/ strtok
+  char str[7000];  // str p/ captura
 
-  scanf("%d %d", &n, &m);
-  getchar();
-  count = n;
-  int arr2d[n][m];
+  scanf("%d %d", &n, &m);  // atribui valores as variáveis n e m
+  getchar();  // limpa buffer
+  count = n;  // atribui valor n ao contador
+  int arr2d[n][m];  // dimensiona matriz de inteiros
 
-  while (count--) {
-    scanf("%[^\n]", str);
-    getchar();
-    posY = n - 1 - count;
-    cPtr = strtok(&str[0], " ");
-    for (int i = 0; cPtr != NULL; ++i) {
-      arr2d[posY][i] = atoi(cPtr);
-      if (arr2d[posY][i] == 42)
-        if (posY > 0 & posY < n - 1)
-          if (cPtr != &str[0] & cPtr != &str[0] + m)
-            pilha.push(coord(posY, i));
-      cPtr = strtok(NULL, " ");
-    }
-  }
+  while (count--) {  // enquanto true?
+    scanf("%[^\n]", str);  // atribui linha a variável str
+    getchar();  // limpa buffer
+    posY = n - 1 - count;  // atribui a posição em Y
+    cPtr = strtok(&str[0], " ");  // strtok p/ cada " "
+    arr2d[posY][0] = atoi(cPtr);  //  atribui valor convertido do atoi
+    for (int i = 1; i < m; ++i) {  // percorre toda a linha
+      cPtr = strtok(NULL, " ");  // próximo token
+      arr2d[posY][i] = atoi(cPtr);  // atribui valor convertido do atoi
+      if (arr2d[posY][i] == 42)  // valor é 42?
+        if (posY < n - 1 & i < m - 1)  // ñ na ult. linha e ñ ult. coluna?
+            pilha.push(coord{posY, i});  // empilha coordenada
+    }  // fim do for
+  }  // fim do while
 
-  while (!pilha.empty()) {
-    count &= 0;
-    xy = pilha.top(); pilha.pop();
-    for (int i  = -1, iy = xy.first; i < 2; ++i) {
-      for (int j = -1, jx = xy.second; j < 2; ++j) {
-        if (arr2d[iy + i][jx + j] == 7) count++;
-        else if (arr2d[iy + i][jx + j] == 42 & i  == 0 & j == 0) count++;
-        else {
-            count &= 0;
-            break;
+  while (!pilha.empty()) {  // enquanto houver coordenada na pilha
+    count &= 0;  // zera o contador
+    xy = pilha.top(); pilha.pop();  // recebe coord. topo pilha e retira
+    for (int i  = -1, iy = xy.y; i < 2; ++i) {  // loop p/ linhas
+      for (int j = -1, jx = xy.x; j < 2; ++j) {  // loop p/ colunas
+        if ((arr2d[iy + i][jx + j] == 7) |  // pos. == 7 ou pos (0,0) == 42?
+            (arr2d[iy + i][jx + j] == 42 & i  == 0 & j == 0)) count++;
+        else {  // condição acima não atendida?
+            count &= 0;  // zera o contador
+            break; // quebra o loop j
         }
-      }
-      if (!(count))
-        break;
-    }
-        
-    if (count == 9) {
-      (xy.first)++; (xy.second)++;
-      break;
-    }
-    xy = std::make_pair(0, 0);
-  }
-  
-  printf("%d %d\n", xy.first, xy.second);
-
-  return 0;
+      }  // fim do for j
+      if (!(count)) break;  // contador == 0 ? quebra o loop i
+    }  // fim do for i
+    if (count == 9) break;  // contador == 9 ? quebra loop while
+    xy = stdXY;  // atribui valores da coordenada padrão
+  }  // fim do while
+  printf("%d %d\n", xy.y + 1, xy.x + 1);  // print das coordenadas
+  return 0;  // fim do programa
 }
