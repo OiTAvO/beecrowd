5030f52054bcf265ded461742b861b93b866202a
Otávio Augusto <otavio.porto@fatec.sp.gov.br>
2019-03-17 15:10:10 -0300

    Solução 1371.cpp

---
+++ 1371.cpp
@@ -0,0 +1,16 @@
+// OAMP - tempo 0.000s
+#include <cstdio>  // entrada e saída padrão
+
+int main() {
+  int i, n;  // indexador e n p/ qtd de netos/quartos
+  char buffer[1000000], *ptr = &buffer[0];  // buffer p/ resp. final e ptr
+  
+  while(scanf("%d", &n) && n != 0) {  // enquanto qtd netos != 0
+    for (i = 1; i*i <= n; ++i)  // loop p/ qtd quartos p/ i² <= qtd quartos
+      ptr += sprintf(ptr, "%d ", i*i);  // concatena resp. no buffer
+      
+    ptr += sprintf(--ptr, "\n");  // concatena new line
+  }
+  printf("%s", buffer);  // printf resp. final
+  return 0;  // fim do programa
+}
