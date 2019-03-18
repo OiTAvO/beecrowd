77474d09ed23848e6c8887d61b61f064109b3963
Otávio Augusto <otavio.porto@fatec.sp.gov.br>
2019-03-17 23:23:28 -0300

    Solução 1041.cpp

---
+++ 1041.cpp
@@ -0,0 +1,22 @@
+// OAMP - tempo 0.000s
+#include <iostream>  // entrada e saída padrçao
+
+using namespace std;
+
+int main() {
+  float x, y;  // variáveis p/ coordenadas
+
+  cin >> x >> y;  // entrada dos dados
+  
+  if (x == 0.0 && y == 0.0) cout << "Origem\n";  // na origem ?
+  else if (y == 0.0) cout << "Eixo X\n";  // no eixo Y ?
+  else if (x == 0.0)cout << "Eixo Y\n";  // no eixo X ?
+  else if (y > 0.0) {  // y nos quadrantes Q1 ou Q2?
+    if (x > 0.0) cout << "Q1\n";  // x no 1º quadrante ?
+    else cout << "Q2\n";  // x no 2º quadrante
+  } else {  // y nos quadrantes Q3 ou Q4
+    if (x < 0.0) cout << "Q3\n";  // x no 3º quadrante ?
+    else cout << "Q4\n";   // x no 4º quadrante
+  } 
+  return 0;  // fim do programa
+}
