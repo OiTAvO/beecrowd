471a4178f512188426308833d8acf1c33ec9daf5
OiTAvO <otavio.porto@fatec.sp.gov.br>
2019-07-23 16:14:04 -0300

    add: This script now allows you to run all tests inside the .\\in folder of the solution.

--- urisln.cmd
+++ urisln.cmd
@@ -2,6 +2,7 @@
 CLS
 SET /P prob=Digite o numero do problema:
 SET /P lang=Digite qual linguagem:
+SET /P qtd=Digite quantidade de testes(ao menos 1):
 SET ext=cpp
 
 CALL :LowerCase lang
@@ -11,25 +12,26 @@ MKDIR ".\%lang%\%prob%\answer"
 MKDIR ".\%lang%\%prob%\in"
 MKDIR ".\%lang%\%prob%\out"
 
-IF EXIST ".\%lang%\%prob%\answer\%ext%_%prob%.ans" (ECHO ".\%lang%\%prob%\answer\%ext%_%prob%.ans ja existe") ELSE (^
-   ECHO. > ".\%lang%\%prob%\answer\%ext%_%prob%.ans"
-   ECHO ".\%lang%\%prob%\answer\%ext%_%prob%.ans" sera criado
+IF EXIST ".\%lang%\%prob%\answer\%prob%_1" (ECHO ".\%lang%\%prob%\answer\%prob%_1 ja existe") ELSE (^
+   FOR /l %%a IN (1,1,%qtd%) DO (^
+       ECHO. > ".\%lang%\%prob%\answer\%prob%_"%%a^
+       & ECHO ".\%lang%\%prob%\answer\%prob%_"%%a foi criado.
+   )
 )
-IF EXIST ".\%lang%\%prob%\in\%ext%_%prob%.in" (ECHO ".\%lang%\%prob%\in\%ext%_%prob%.in ja existe") ELSE (^
-   ECHO. > ".\%lang%\%prob%\in\%ext%_%prob%.in"
-   ECHO ".\%lang%\%prob%\in\%ext%_%prob%.in" sera criado
+IF EXIST ".\%lang%\%prob%\in\%prob%_1" (ECHO ".\%lang%\%prob%\in\%ext%_%prob%_1 ja existe") ELSE (^
+   FOR /l %%a IN (1,1,%qtd%) DO (^
+       ECHO. > ".\%lang%\%prob%\in\%prob%_"%%a^
+       & ECHO ".\%lang%\%prob%\in\%prob%_"%%a foi criado.
+   )
 )
 
 IF EXIST ".\%lang%\%prob%\%ext%_%prob%.%ext%" (ECHO "%ext%_%prob%.%ext% ja existe") ELSE (^
-   IF %lang%==cpp (SET flag=True) ELSE (SET flag=False)
-   IF %lang%==d (SET flag=True)
-   IF %flag%==True (CALL :CppDFile %lang%, %prob%)
-   IF %lang%==python (CALL :PyFile %lang%, %prob%)
-   IF %lang%==haskell (CALL :HsFile %lang%, %prob%)
+   IF %lang%==cpp CALL :CppFile %lang%, %prob% ELSE ()
+   IF %lang%==d CALL :DFile %lang%, %prob% ELSE ()
+   IF %lang%==python CALL :PyFile %lang%, %prob% ELSE ()
+   IF %lang%==haskell CALL :HsFile %lang%, %prob% ELSE ()
    ECHO O arquivo ".\%lang%\%prob%\%ext%_%prob%.%ext%" foi criado
 )
-
-
 
 PAUSE
 
@@ -51,8 +53,8 @@ IF %~1==cpp (SET %~2%=cpp)
 IF %~1==haskell (SET %~2%=hs)
 GOTO:EOF
 
-:CppDFile
-:: Subroutine to generate init file for d or cpp
+:CppFile
+:: Subroutine to generate init file for cpp
    FOR /f %%i IN ('git config user.name') DO SET username=%%i
 
    ECHO /* > ".\%lang%\%prob%\%ext%_%prob%.%ext%"
@@ -66,38 +68,60 @@ GOTO:EOF
    ECHO. >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
 
    :: main function for cpp or d
-   IF %~1==cpp (^
-      ECHO #include^<iostream^> >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
-      ECHO. >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
-      ECHO using namespace std; >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
-      ECHO. >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
-      ECHO int main(^) { >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
-      ECHO.    cout ^<^< "Ola Mundo\n"; >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
-      ECHO.    return 0; >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
-      ECHO } >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
-      ECHO. >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
+   ECHO #include^<iostream^> >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
+   ECHO. >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
+   ECHO using namespace std; >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
+   ECHO. >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
+   ECHO int main(^) { >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
+   ECHO.    cout ^<^< "Ola Mundo\n"; >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
+   ECHO.    return 0; >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
+   ECHO } >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
+   ECHO. >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
 
-      :: generate runsln for cpp
-      ECHO @ECHO OFF > ".\%lang%\%prob%\runsln.cmd"     
-      ECHO g++ -std=c++11 -O2 ".\%ext%_%prob%.%ext%" -o ".\%ext%_%prob%.exe" >> ".\%lang%\%prob%\runsln.cmd"
-      ECHO ".\%ext%_%prob%.exe" ^< ".\in\%ext%_%prob%.in" ^> ".\out\%ext%_%prob%.out" >> ".\%lang%\%prob%\runsln.cmd"
-      ECHO FC ".\out\%ext%_%prob%.out" ".\answer\%ext%_%prob%.ans" >> ".\%lang%\%prob%\runsln.cmd"
-      ECHO PAUSE >> ".\%lang%\%prob%\runsln.cmd"
-   ) ELSE (^
-      ECHO import std.stdio; >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
-      ECHO. >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
-      ECHO void main(^) >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
-      ECHO { >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
-      ECHO.    writeln("Ola Mundo"^); >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
-      ECHO } >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
-      ECHO. >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
+   :: generate runsln for cpp
+   ECHO @ECHO OFF > ".\%lang%\%prob%\runsln.cmd"
+   ECHO g++ -std=c++11 -O2 ".\%ext%_%prob%.%ext%" -o ".\%ext%_%prob%.exe" >> ".\%lang%\%prob%\runsln.cmd"
+   ECHO FOR /F %%%%i IN ('DIR ".\in" /B') DO (^ >> ".\%lang%\%prob%\runsln.cmd"
+   ECHO     ".\%ext%_%prob%.exe" ^< ".\in\"%%%%i ^> ".\out\"%%%%i >> ".\%lang%\%prob%\runsln.cmd"   
+   ECHO     FC /N ".\out\"%%%%i ".\answer\"%%%%i^^ >> ".\%lang%\%prob%\runsln.cmd"
+   ECHO     ECHO Proximo...^^ >> ".\%lang%\%prob%\runsln.cmd"
+   ECHO     PAUSE^) >> ".\%lang%\%prob%\runsln.cmd"
+   ECHO ECHO FIM DOS TESTES. >> ".\%lang%\%prob%\runsln.cmd"
+   ECHO PAUSE >> ".\%lang%\%prob%\runsln.cmd"
+GOTO:EOF
 
-      :: generate runsln for d
-      ECHO @ECHO OFF > ".\%lang%\%prob%\runsln.cmd"      
-      ECHO rdmd ".\%ext%_%prob%.%ext%" ^< ".\in\%ext%_%prob%.in" ^> ".\out\%ext%_%prob%.out" >> ".\%lang%\%prob%\runsln.cmd"
-      ECHO FC ".\out\%ext%_%prob%.out" ".\answer\%ext%_%prob%.ans" >> ".\%lang%\%prob%\runsln.cmd"
-      ECHO PAUSE >> ".\%lang%\%prob%\runsln.cmd"     
-   )
+
+:DFile
+:: Subroutine to generate init file for d
+   FOR /f %%i IN ('git config user.name') DO SET username=%%i
+
+   ECHO /* > ".\%lang%\%prob%\%ext%_%prob%.%ext%"
+   ECHO.    author: %username% >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
+   ECHO.    problem_name:  >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
+   ECHO.    problem_number: %~2 >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
+   ECHO.    category:  >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
+   ECHO.    difficulty_level:  >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
+   ECHO.    link: https://www.urionlinejudge.com.br/judge/pt/problems/view/%~2 >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
+   ECHO */ >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
+   ECHO. >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
+
+   ECHO import std.stdio; >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
+   ECHO. >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
+   ECHO void main(^) >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
+   ECHO { >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
+   ECHO.    writeln("Ola Mundo"^); >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
+   ECHO } >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
+   ECHO. >> ".\%lang%\%prob%\%ext%_%prob%.%ext%"
+
+   :: generate runsln for d
+   ECHO @ECHO OFF > ".\%lang%\%prob%\runsln.cmd"      
+   ECHO FOR /F %%%%i IN ('DIR ".\in" /B') DO (^ >> ".\%lang%\%prob%\runsln.cmd"
+   ECHO     rdmd ".\%ext%_%prob%.%ext%" ^< ".\in\"%%%%i ^> ".\out\"%%%%i >> ".\%lang%\%prob%\runsln.cmd"   
+   ECHO     FC /N ".\out\"%%%%i ".\answer\"%%%%i^^ >> ".\%lang%\%prob%\runsln.cmd"
+   ECHO     ECHO Proximo...^^ >> ".\%lang%\%prob%\runsln.cmd"
+   ECHO     PAUSE^) >> ".\%lang%\%prob%\runsln.cmd"
+   ECHO ECHO FIM DOS TESTES. >> ".\%lang%\%prob%\runsln.cmd"
+   ECHO PAUSE >> ".\%lang%\%prob%\runsln.cmd"
 GOTO:EOF
 
 :PyFile
@@ -117,8 +141,12 @@ GOTO:EOF
 
    :: generate runsln for python
    ECHO @ECHO OFF > ".\%lang%\%prob%\runsln.cmd"   
-   ECHO python ".\%ext%_%prob%.%ext%" ^< ".\in\%ext%_%prob%.in" ^> ".\out\%ext%_%prob%.out" >> ".\%lang%\%prob%\runsln.cmd"
-   ECHO FC ".\out\%ext%_%prob%.out" ".\answer\%ext%_%prob%.ans" >> ".\%lang%\%prob%\runsln.cmd"
+   ECHO FOR /F %%%%i IN ('DIR ".\in" /B') DO (^ >> ".\%lang%\%prob%\runsln.cmd"
+   ECHO     python ".\%ext%_%prob%.%ext%" ^< ".\in\"%%%%i ^> ".\out\"%%%%i >> ".\%lang%\%prob%\runsln.cmd"   
+   ECHO     FC /N ".\out\"%%%%i ".\answer\"%%%%i^^ >> ".\%lang%\%prob%\runsln.cmd"
+   ECHO     ECHO Proximo...^^ >> ".\%lang%\%prob%\runsln.cmd"
+   ECHO     PAUSE^) >> ".\%lang%\%prob%\runsln.cmd"
+   ECHO ECHO FIM DOS TESTES. >> ".\%lang%\%prob%\runsln.cmd"
    ECHO PAUSE >> ".\%lang%\%prob%\runsln.cmd"
 GOTO:EOF
 
@@ -142,7 +170,11 @@ GOTO:EOF
    ECHO ghc -O ".\%ext%_%prob%.%ext%" >> ".\%lang%\%prob%\runsln.cmd"
    ECHO ERASE ".\%ext%_%prob%.hi" >> ".\%lang%\%prob%\runsln.cmd"
    ECHO ERASE ".\%ext%_%prob%.o" >> ".\%lang%\%prob%\runsln.cmd"
-   ECHO ".\%ext%_%prob%.exe" ^< ".\in\%ext%_%prob%.in" ^> ".\out\%ext%_%prob%.out" >> ".\%lang%\%prob%\runsln.cmd"
-   ECHO FC ".\out\%ext%_%prob%.out" ".\answer\%ext%_%prob%.ans" >> ".\%lang%\%prob%\runsln.cmd"
+   ECHO FOR /F %%%%i IN ('DIR ".\in" /B') DO (^ >> ".\%lang%\%prob%\runsln.cmd"
+   ECHO     ".\%ext%_%prob%.exe" ^< ".\in\"%%%%i ^> ".\out\"%%%%i >> ".\%lang%\%prob%\runsln.cmd"   
+   ECHO     FC /N ".\out\"%%%%i ".\answer\"%%%%i^^ >> ".\%lang%\%prob%\runsln.cmd"
+   ECHO     ECHO Proximo...^^ >> ".\%lang%\%prob%\runsln.cmd"
+   ECHO     PAUSE^) >> ".\%lang%\%prob%\runsln.cmd"
+   ECHO ECHO FIM DOS TESTES. >> ".\%lang%\%prob%\runsln.cmd"
    ECHO PAUSE >> ".\%lang%\%prob%\runsln.cmd"
 GOTO:EOF
