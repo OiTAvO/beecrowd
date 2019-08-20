71ccd411bb5fb91a47f9dadb8df0b63448bbb7ba
OiTAvO <otavio.porto@fatec.sp.gov.br>
2019-08-19 23:19:27 -0300

    refactor: urisln.sh

--- urisln.sh
+++ urisln.sh
@@ -9,21 +9,17 @@ do
     esac
 done
 
-if [ -z "$prob" ]
-then
-    echo "Digite o numero do problema: "
-    read prob
+if [ -z "$prob" ]; then
+    echo "Digite o numero do problema: "; read prob
 fi
 
-if [ -z "$lang" ]
-then
-    echo "Digite qual linguagem: "
-    read lang
+if [ -z "$lang" ]; then
+    echo "Digite qual linguagem: "; read lang
 fi
 
-if [ -z "$qtd" ]
-then
-    echo "Digite a quantidade de teste(ao menos 1): "
-    read qtd
+if [ -z "$qtd" ]; then
+    echo "Digite a quantidade de teste(ao menos 1): "; read qtd
 fi
+
+lang=`echo "$lang" | tr '[:upper:]' '[:lower:]'`
 
