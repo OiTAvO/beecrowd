b5b6fa4b3c0a53346db78d5b5563be49ded9fca1
OiTAvO <otavio.porto@fatec.sp.gov.br>
2019-10-02 14:58:42 -0300

    refactor: Blank lines deleted.

--- cpp/2422/cpp_2422.cpp
+++ cpp/2422/cpp_2422.cpp
@@ -14,11 +14,8 @@ using namespace std;
 int main() {
     int n, soma, vi[100000];
     
-    scanf("%d", &n);
-    
-    for (int i = 0; i < n; ++i)
-        scanf("%d", &vi[i]);
-
+    scanf("%d", &n);    
+    for (int i = 0; i < n; ++i) scanf("%d", &vi[i]);
     scanf("%d", &soma);
    
     for(int i = 0;;++i)
