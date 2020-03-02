ce992552de4f35b9372da6e9909a241163d36e62
OiTAvO <otavio.porto@fatec.sp.gov.br>
2020-03-02 01:19:26 -0300

    urisln.sh almost complete

--- urisln.sh
+++ urisln.sh
@@ -14,28 +14,71 @@ function langExt() {
 }
 
 function slnDoc() {
-    src="./$1/$2/$3_$2.$3"
+    src="./$lang/$prob/$1_$prob.$1"
     username=`git config user.name`
-    echo -e "    author: $usename\r\n" >> "$src"
-    echo -e "    problem_name: \r\n" >> "$src"
-    echo -e "    problem_number: $2\r\n" >> "$src"
-    echo -e "    category: \r\n" >> "$src"
-    echo -e "    difficulty_level: \r\n" >> "$src"
-    echo -e "    link: https://www.urionlinejudge.com.br/judge/pt/problems/view/$2\r\n" >> "$src"
+    echo -e "$(iniDoc $1)" >> "$src"
+    echo -e "    author: $usename" >> "$src"
+    echo -e "    problem_name: " >> "$src"
+    echo -e "    problem_number: $prob" >> "$src"
+    echo -e "    category: " >> "$src"
+    echo -e "    difficulty_level: " >> "$src"
+    echo -e "    link: https://www.urionlinejudge.com.br/judge/pt/problems/view/$prob" >> "$src"
+    echo -e "$(endDoc $1)\n" >> "$src"
 }
 
+function iniDoc() {
+    case $1 in
+	py ) echo '"""' ;;
+	cpp ) echo '/*' ;;
+	d ) echo '/*' ;;
+	hs ) echo '{--' ;;
+    esac
+}
 
+function endDoc() {
+    case $1 in
+	py ) echo '"""' ;;
+	cpp ) echo '*/' ;;
+	d ) echo '*/' ;;
+	hs ) echo '--}' ;;
+    esac
+}
+
+function command() {
+    case $1 in
+	py ) echo '' ;;
+	cpp ) echo 'g++ -std=c++11 -O2 ./'$ext'_'$prob.$ext -o ./$ext'_'$prob ;;
+	d ) echo '' ;;
+	hs ) echo 'ghc -O -no-keep-hi-files -no-keep-o-files '$ext'_'$prob ;;
+    esac    
+}
+
+function exeorscript() {
+    case $1 in
+	py ) echo 'python3 ./'$ext'_'$prob.$ext ;;
+	cpp ) echo './'$ext'_'$prob ;;
+	d ) echo 'rdmd ./'$$ext'_'$prob.$ext ;;
+	hs ) echo './'$ext'_'$prob ;;
+    esac        
+}
+
+function runsln() {
+    src="./$lang/$prob/runsln.sh"
+    if [ -n "$(command $1)" ]; then
+	echo -e "$(command $1)" >> "$src"
+    fi
+    echo -e "for f in ./in/*; do" >> "$src"
+    echo -e "    $(exeorscript $1) < ./in/\${f##*/} > ./out/\${f##*/}" >> "$src"
+    echo -e "    diff ./out/\${f##*/} ./answer/\${f##*/}" >> "$src"
+    echo -e "    echo 'Próximo...'" >> "$src"
+    echo -e "    read pause" >> "$src"
+    echo -e "done" >> "$src"
+    echo -e "echo 'FIM DOS TESTES'" >> "$src"
+    echo -e "read pause" >> "$src"
+    chmod 777 $src
+}
 
 function main() {
-    while getopts "p:l:q:" opt
-    do
-        case "$opt" in
-          p ) prob="$OPTARG" ;;
-          l ) lang="$OPTARG" ;;
-          q ) qtd="$OPTARG" ;;
-        esac
-    done
-
     if [ -z "$prob" ]; then
         echo "Digite o numero do problema: "; read prob
     fi
@@ -48,14 +91,30 @@ function main() {
         echo "Digite a quantidade de teste(ao menos 1): "; read qtd
     fi
 
-
+    lang=$(toLower $lang)
+    
     # Cria diretorios
-    #mkdir -p "./$lang/$prob/answer"
-    #mkdir -p "./$lang/$prob/in"
-    #mkdir -p "./$lang/$prob/out"
+    mkdir -p "./$lang/$prob/answer"
+    mkdir -p "./$lang/$prob/in"
+    mkdir -p "./$lang/$prob/out"
 
+    for q in $(seq 1 $qtd); do
+	touch "./$lang/$prob/answer/$prob_$q"
+	touch "./$lang/$prob/in/$prob_$q"	
+    done
+    
     ext=$(langExt $lang)
-    #slnDoc $lang $prob $ext
+    $(slnDoc $ext)
+    $(runsln $ext)
 }
+
+while getopts "p:l:q:" opt
+do
+    case "$opt" in
+        p ) prob="$OPTARG" ;;
+        l ) lang="$OPTARG" ;;
+        q ) qtd="$OPTARG" ;;
+    esac
+done
 
 main #entry point
